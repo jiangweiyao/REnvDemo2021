c84b3ec9be053ee0264b5e3af956b862eec60993
Yao <pqq7@login02.aspen.biotech.cdc.gov>
2021-01-27 16:43:36 -0500

    first commit

---
+++ TestCode.R
@@ -0,0 +1,10 @@
+install.packages("tidyverse")
+install.packages("renv")
+install.packages("BiocManager")
+BiocManager::install("DESeq2")
+
+library(tidyverse)
+
+library(DESeq2)
+library(MASS)
+library(grid)
